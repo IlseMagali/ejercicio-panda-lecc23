2bc682ddd733a3626bb1ed3a51556d0a016870d6
IlseMagali <ilsemke@gmail.com>
2017-04-18 10:57:11 +0000

    v5 ejercicio pandas

--- assets/js/panda.js
+++ assets/js/panda.js
@@ -1,4 +1,16 @@
 //alert("hola");
-
+var botonCerrar = document.getElementsByTagName("span");
 var imagenes = document.getElementsByClassName("panda");
 console.log(imagenes);
+
+for (var i = 0; i < botonCerrar.length;i++){
+  botonCerrar[i].addEventListener("click", function(){
+    for (var i = 0; i < imagenes.length;i++){
+      imagenes[i].style = this.style;
+      this.style.display = "none";
+    }
+
+  });
+}
+
+//
